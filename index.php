b83f10a5b9c035a44ad9bb208f09800d550bccfd
Nguyen Trong Tinh <trongtinh2k@gmail.com>
2021-10-15 16:38:01 +0700

    a

--- index.php
+++ index.php
@@ -200,7 +200,7 @@ $result = $product->Search_Paginate($start, $limit,$keyword);
                                 <div class="product-f-image">
                                     <img src="pictures/<?php echo $value['ImageUrl'] ?>" alt="" style="width:220px;height:280px;">
                                     <div class="product-hover">
-                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to
+                                        <a href="cart.php" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to
                                             cart</a>
                                             <a href="single-product.php?id=<?=  $value['ProductID']?>" class="view-details-link"><i class="fa fa-link"></i> See details</a>
                                     </div>
@@ -290,7 +290,7 @@ $result = $product->Search_Paginate($start, $limit,$keyword);
                     <h2 class="product-wid-title">Top Sellers</h2>
                     <a href="" class="wid-view-more">View All</a>
                     <div class="single-wid-product">
-                        <a href="single-product.html"><img src="img/product-thumb-1.jpg" alt=""
+                        <a href="single-product.html"><img src="img/product-thumb-4.jpg" alt=""
                                                            class="product-thumb"></a>
                         <h2><a href="single-product.html">Sony Smart TV - 2015</a></h2>
                         <div class="product-wid-rating">
@@ -306,7 +306,7 @@ $result = $product->Search_Paginate($start, $limit,$keyword);
                         </div>
                     </div>
                     <div class="single-wid-product">
-                        <a href="single-product.html"><img src="img/product-thumb-2.jpg" alt=""
+                        <a href="single-product.html"><img src="img/product-thumb-4.jpg" alt=""
                                                            class="product-thumb"></a>
                         <h2><a href="single-product.html">Apple new mac book 2015</a></h2>
                         <div class="product-wid-rating">
@@ -322,7 +322,7 @@ $result = $product->Search_Paginate($start, $limit,$keyword);
                         </div>
                     </div>
                     <div class="single-wid-product">
-                        <a href="single-product.html"><img src="img/product-thumb-3.jpg" alt=""
+                        <a href="single-product.html"><img src="img/product-thumb-4.jpg" alt=""
                                                            class="product-thumb"></a>
                         <h2><a href="single-product.html">Apple new i phone 6</a></h2>
                         <div class="product-wid-rating">
@@ -360,7 +360,7 @@ $result = $product->Search_Paginate($start, $limit,$keyword);
                         </div>
                     </div>
                     <div class="single-wid-product">
-                        <a href="single-product.html"><img src="img/product-thumb-1.jpg" alt=""
+                        <a href="single-product.html"><img src="img/product-thumb-4.jpg" alt=""
                                                            class="product-thumb"></a>
                         <h2><a href="single-product.html">Sony Smart Air Condtion</a></h2>
                         <div class="product-wid-rating">
@@ -376,7 +376,7 @@ $result = $product->Search_Paginate($start, $limit,$keyword);
                         </div>
                     </div>
                     <div class="single-wid-product">
-                        <a href="single-product.html"><img src="img/product-thumb-2.jpg" alt=""
+                        <a href="single-product.html"><img src="img/product-thumb-4.jpg" alt=""
                                                            class="product-thumb"></a>
                         <h2><a href="single-product.html">Samsung gallaxy note 4</a></h2>
                         <div class="product-wid-rating">
@@ -398,7 +398,7 @@ $result = $product->Search_Paginate($start, $limit,$keyword);
                     <h2 class="product-wid-title">Top New</h2>
                     <a href="#" class="wid-view-more">View All</a>
                     <div class="single-wid-product">
-                        <a href="single-product.html"><img src="img/product-thumb-3.jpg" alt=""
+                        <a href="single-product.html"><img src="img/product-thumb-4.jpg" alt=""
                                                            class="product-thumb"></a>
                         <h2><a href="single-product.html">Apple new i phone 6</a></h2>
                         <div class="product-wid-rating">
@@ -430,7 +430,7 @@ $result = $product->Search_Paginate($start, $limit,$keyword);
                         </div>
                     </div>
                     <div class="single-wid-product">
-                        <a href="single-product.html"><img src="admin/images/product-thumb-1.jpg" alt=""
+                        <a href="single-product.html"><img src="img/product-thumb-4.jpg" alt=""
                                                            class="product-thumb"></a>
                         <h2><a href="single-product.html">Sony playstation microsoft</a></h2>
                         <div class="product-wid-rating">
