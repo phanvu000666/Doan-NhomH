25ab774d9b93026fabd93d7ffb910be397c73b29
Hoang Vu <phanvu000666@gmail.com>
2021-11-02 08:44:32 +0700

    update theme auth

--- dangki.php
+++ dangki.php
@@ -31,30 +31,39 @@
 
 <style>
     .wrap-login100{
-        background: #DDA0DD;
+        background-image: url('pictures/upload/ROGTHEME.jpg');
     }
 </style>
 
-<div class="container-login100" style="background-image: url('pictures/bg-01.jpg');">
+<div class="container-login100" style="background-image: url('pictures/upload/ROGTHEME.jpg');">
     <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
         <form class="login100-form validate-form">
 			<span class="login100-form-title p-b-37">
-				Sign In
+				Sign Up
 			</span>
 
-            <div class="wrap-input100 validate-input m-b-20" data-validate="Enter username or email">
-                <input class="input100" type="text" name="username" placeholder="username or email">
+            <div class="wrap-input100 validate-input m-b-20" data-validate="Enter full name">
+                <input class="input100" type="text" name="username" placeholder="Fullname">
+                <span class="focus-input100"></span>
+            </div>
+
+            <div class="wrap-input100 validate-input m-b-25" data-validate = "Enter Username">
+                <input class="input100" type="password" name="pass" placeholder="Username">
+                <span class="focus-input100"></span>
+            </div>
+            <div class="wrap-input100 validate-input m-b-20" data-validate="Enter email">
+                <input class="input100" type="text" name="username" placeholder="Email">
                 <span class="focus-input100"></span>
             </div>
 
             <div class="wrap-input100 validate-input m-b-25" data-validate = "Enter password">
-                <input class="input100" type="password" name="pass" placeholder="password">
+                <input class="input100" type="password" name="pass" placeholder="Password">
                 <span class="focus-input100"></span>
             </div>
 
             <div class="container-login100-form-btn">
                 <button class="login100-form-btn">
-                    Sign In
+                    Sign Up
                 </button>
             </div>
 
@@ -76,7 +85,7 @@
 
             <div class="text-center">
                 <a href="dangnhap.php" class="txt2 hov1">
-                    Sign In !
+                    Do you have an account ? Log in !
                 </a>
             </div>
         </form>
--- dangnhap.php
+++ dangnhap.php
@@ -31,11 +31,11 @@
 
 <style>
     .wrap-login100{
-        background: #DDA0DD;
+        background-image: url('pictures/upload/ROGTHEME.jpg');
     }
 </style>
 
-<div class="container-login100" style="background-image: url('pictures/bg-01.jpg');">
+<div class="container-login100" style="background-image: url('pictures/upload/ROGTHEME.jpg');">
     <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
         <form class="login100-form validate-form">
 			<span class="login100-form-title p-b-37">
@@ -43,12 +43,12 @@
 			</span>
 
             <div class="wrap-input100 validate-input m-b-20" data-validate="Enter username or email">
-                <input class="input100" type="text" name="username" placeholder="username or email">
+                <input class="input100" type="text" name="username" placeholder="Username or email">
                 <span class="focus-input100"></span>
             </div>
 
             <div class="wrap-input100 validate-input m-b-25" data-validate = "Enter password">
-                <input class="input100" type="password" name="pass" placeholder="password">
+                <input class="input100" type="password" name="pass" placeholder="Password">
                 <span class="focus-input100"></span>
             </div>
 
