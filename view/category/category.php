a1c5aa28aa20b4b4854666add3cf53146c2c8e8d
Nguyen Trong Tinh <trongtinh2k@gmail.com>
2021-11-23 04:34:10 +0700

    header project

--- view/category/category.php
+++ view/category/category.php
@@ -2,6 +2,7 @@
 
 //use SmartWeb\Category;
 use SmartWeb\Category;
+
 $ds = DIRECTORY_SEPARATOR;
 $base_dir = realpath(dirname(__FILE__)  . $ds . '..') . $ds;
 
@@ -9,5 +10,5 @@ $cate = Category::getInstance();
 $resultc = $cate->getCategory();
 // var_dump($result);
 foreach ($resultc as $row) {
-    echo "<a href=\"index.php?mod=category&act=resultcategory&id={$row['CategoryID']}\">{$row['CategoryName']}</a>";
+    echo "<li class='menu-item menu-item-type-post_type menu-item-object-page menu-item-20527'><a class='nav-link' href=\"index.php?mod=category&act=resultcategory&id={$row['CategoryID']}\">{$row['CategoryName']}</a></li>";
 }
