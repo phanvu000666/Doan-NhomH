d2b321e5a07f0a619cee70e22b70e53e7c5e2472
thanhquan1306 <19211tt0882@mail.tdc.edu.vn>
2021-12-01 07:24:38 +0700

    commit register

--- model/config.php
+++ model/config.php
@@ -3,8 +3,4 @@ const SEVERNAME = "localhost";
 const DATABASE = "smart-web";
 const USERNAME = "root";
 const PASSWORD = "";
-<<<<<<< HEAD
 const PORT = "3306";
-=======
-const PORT = "3307";
->>>>>>> origin/main
