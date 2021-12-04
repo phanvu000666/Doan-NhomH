d1dd2c280cd13c13393b7031c7b748213cd89b82
Hoang Vu <phanvu000666@gmail.com>
2021-12-05 05:48:17 +0700

    update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once 'Controller/FactoryPattern.php';
 $factory = new FactoryPattern();
 $product = $factory->make('product');
 $banner = $factory->make('banner');
-$baner = $banner->getBanner();
+$ban = $banner->getBanner();
 $products = $product->getData();
 $result = $product->getSPNew();
 
