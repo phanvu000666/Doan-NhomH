c72d603504418452050d0c37605fa08d30edc0d0
Thor <80993152+QuocTuann@users.noreply.github.com>
2021-12-11 20:45:28 +0700

    Create TestCategory

--- admin/protype/category.php
+++ admin/protype/category.php
@@ -14,8 +14,8 @@ class Category extends Product
 
     public static function getInstance()
     {
-        if (empty($phone)) {
-            self::$phone = new self(self::$db);
+        if (empty(static::$phone)) {
+            self::$phone = new self(new DBPDO());
         }
         return self::$phone;
     }
@@ -29,8 +29,11 @@ class Category extends Product
 
     public function getOne($id)
     {
-        $sql = "SELECT * FROM $this->tbl_cate WHERE $this->CategoryID = $id";
-        $is_finished =  $this->db->select($sql);
+        $is_finished = false;
+        if (is_int($id)) {
+            $sql = "SELECT * FROM $this->tbl_cate WHERE $this->CategoryID = $id";
+            $is_finished =  $this->db->select($sql);
+        }
         return $is_finished;
     }
 
@@ -43,48 +46,54 @@ class Category extends Product
 
     public function updateOne($input)
     {
-        if (
-            isset($input[$this->CategoryID]) &&
-            isset($input[$this->CategoryName]) &&
-            isset($input[$this->Position]) &&
-            isset($input[$this->Version])
-        ) {
-            $data = [
-                $this->CategoryID => $input[$this->CategoryID],
-                $this->CategoryName => $input[$this->CategoryName],
-                $this->Position => $input[$this->Position],
-                $this->Version => $input[$this->Version]
-            ];
+        $is_finished = false;
+        if (is_array($input) && isset($_SESSION["Hash"]) && CSRFToken::CompareTokens($input["Hash"], $_SESSION["Hash"])) {
+            if (
+                isset($input[$this->CategoryID]) &&
+                isset($input[$this->CategoryName]) &&
+                isset($input[$this->Position]) &&
+                isset($input[$this->Version])
+            ) {
+                $data = [
+                    $this->CategoryID => $input[$this->CategoryID],
+                    $this->CategoryName => $input[$this->CategoryName],
+                    $this->Position => $input[$this->Position],
+                    $this->Version => $input[$this->Version]
+                ];
 
-            $sql = "UPDATE $this->tbl_cate SET
+                $sql = "UPDATE $this->tbl_cate SET
                 $this->CategoryName = :$this->CategoryName,
                 $this->Position = :$this->Position,
                 $this->Version = :$this->Version
                 WHERE $this->CategoryID = :$this->CategoryID";
 
-            $is_finished =  $this->db->notSelect($sql, $data);
-            return $is_finished;
-        } else {
-            return [
-                'success' => false,
-                'message' => "Parameters is not accord!"
-            ];
-            die();
+                $is_finished =  $this->db->notSelect($sql, $data);
+            } else {
+                return [
+                    'success' => false,
+                    'message' => "Parameters is not accord!"
+                ];
+                die();
+            }
         }
+        return $is_finished;
     }
 
     public function insertOne($input)
     {
-        if (
-            isset($input[$this->CategoryName]) &&
-            isset($input[$this->Position])
-        ) {
-            $data = [
-                $this->CategoryName => $input[$this->CategoryName],
-                $this->Position => $input[$this->Position],
-                $this->Version => 0
-            ];
-            $sql = "INSERT $this->tbl_cate 
+        $is_finished = false;
+        if (is_array($input) && count($input) === 2) {
+
+            if (
+                isset($input[$this->CategoryName]) &&
+                isset($input[$this->Position])
+            ) {
+                $data = [
+                    $this->CategoryName => $input[$this->CategoryName],
+                    $this->Position => $input[$this->Position],
+                    $this->Version => 0
+                ];
+                $sql = "INSERT $this->tbl_cate 
             (
             $this->CategoryName,
             $this->Position,
@@ -96,16 +105,45 @@ class Category extends Product
             :Position,
             :Version
             )";
-            $is_finished =  $this->db->notSelect($sql, $data);
-            return $is_finished;
-        } else {
-            return [
-                'success' => false,
-                'message' => "Parameters is not accord!"
-            ];
-            die();
+                $is_finished =  $this->db->notSelect($sql, $data);
+            } else {
+                return [
+                    'success' => false,
+                    'message' => "Parameters is not accord!"
+                ];
+                die();
+            }
         }
+        return $is_finished;
+    }
+    public function getVersion($id)
+    {
+        $sql = "SELECT categories.Version FROM categories 
+        WHERE categories.CategoryID=:CategoryID";
 
-       
+        $params['CategoryID'] = $id;
+        $result = $this->db->select($sql, $params);
+        return $result;
+    }
+    public function setVersion($id)
+    {
+        $is_finished = false;
+        if (is_int($id)) {
+
+            $sql = "UPDATE categories SET Version = Version + 1 WHERE CategoryID =:CategoryID";
+
+            $param = ["CategoryID" => $id];
+            $is_finished =  $this->db->notSelect($sql, $param);
+        }
+        return $is_finished;
+    }
+    public function startTransaction()
+    {
+        $this->db->getConnect()->beginTransaction();
+    }
+
+    public function rollBack()
+    {
+        $this->db->getConnect()->rollback();
     }
 }
